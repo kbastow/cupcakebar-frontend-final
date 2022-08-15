3eafc1869e8b0a09ae5e4dea91e1ea66e33e7aa5
Kate <bastowkate@gmail.com>
2022-08-16 00:28:54 +1000

    Render orders and base styling for orders table

--- src/views/pages/orders.js
+++ src/views/pages/orders.js
@@ -26,26 +26,50 @@ class OrdersView {
   }
 
   render(){
-    const template = html`
-      <cb-app-header user="${JSON.stringify(Auth.currentUser)}"></cb-app-header>
 
-      <div class="orders page-content calign">
-        <h1>Customer Orders</h1>
+  const template = html`
+
+  <cb-app-header user="${JSON.stringify(Auth.currentUser)}"></cb-app-header>
+    
+      <div class="page-content calign">
+        <h1>CUSTOMER ORDERS</h1>
+
         <div class="orders-grid">
-          ${this.orders == null ? html`
-          <div class="loading">
-          <img src="../../images/loading-animation.gif"/> </div>
+          <table class="orders-table calign">
+              <tr>
+                <th>Order Number</th>
+                <th>Customer</th>
+                <th>Order Summary</th>
+                <th>Total</th>
+                <th>Date Ordered</th>
+                <th>Status</th>
+              </tr>
+          ${
+            this.orders == null ? html`
+          <div class="loading calign">
+            <img src="../../images/loading-animation.gif"/> 
+          </div>
+
+
           ` : html`
-            ${this.orders.map(
+          ${this.orders.length == 0 
+          ? html`<h2> No orders to fulfill</h2>
+          <sl-button class="back-btn" type="primary" @click=${() => gotoRoute('/')}>HOME</sl-button>`
+
+            : this.orders.map(
               (order) => html`
-            <table
-                id="${order._id}"
-                user="${order.user_id}"
-                orderSummary="${order.orderSummary}"
-                orderStatus="${order.orderStatus}"
-                createdAt="${order.createdAt}"
-            ></table>
-            `)}
+            
+              <tr class="customer-order">
+                <td class="order-number">${order._id}</td>
+                <td class="order-customer">${order.user}</td>
+                <td class="order-summary">${order.products}</td>
+                <td class="order-total">${order.total}</td>
+                <td class="order-date">${order.createdAt}</td>
+                <td class="order-status">${order.status}</td>
+              </tr>
+          </table>            
+            `
+            )}
           `}
         </div>
       </div>
